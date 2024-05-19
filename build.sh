ac534c123b4fb9d2bf0ebc2044995ef1f86c87ba
Puliuvarthi.Mahesh <134507390+MaheshPulivarthi18@users.noreply.github.com>
2024-05-20 01:45:27 +0530

    fixing deployement

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+chmod +x example/static/ago.exe
+chmod +w example/static/bankFile.txt
+chmod +w example/static/transactionsFile.txt
