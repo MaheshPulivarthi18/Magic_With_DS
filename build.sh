81f548ca4d9d1db362df5078c760b90ebf947de5
Puliuvarthi.Mahesh <134507390+MaheshPulivarthi18@users.noreply.github.com>
2024-05-20 18:11:58 +0530

    fixedbuild.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
-chmod +x .example/static/ago.exe
-chmod +w .example/static/bankFile.txt
-chmod +w .example/static/transactionsFile.txt
+
